6129cecbd1bf76b26b95d10fea8250ed4511b5df
Umar Farooq <umar.great.01@gmail.com>
2023-11-02 16:10:09 +0530

    login signup frontend 2/11

--- htdocs/users/signup.php
+++ htdocs/users/signup.php
@@ -1,33 +1,156 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<link href="../css/signup.css" rel="stylesheet">
-<!------ Include the above in your HEAD tag ---------->
+<!DOCTYPE html>
+<html lang="en">
 
-<div class="sidenav">
-         <div class="login-main-text align-items-center">
-            <br>
-            <br>
-         <h1 class="text-white">Cloud Labs</span></h1>
-            <p>Login or register from here to access.</p>
-         </div>
-      </div>
-      <br>
-      <div class="main">
-         <div class="col-md-6 col-sm-12">
-            <div class="login-form">
-               <form>
-                  <div class="form-group">
-                     <label>User Name</label>
-                     <input type="text" class="form-control" placeholder="User Name">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Signup - Cloud Labs</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="/assets/img/favicon3.png" rel="icon">
+  <link href="/assets/img/favicon3.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/bootstrap-icons.css" rel="stylesheet">
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="../images/favicon.ico" />
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="../css/bootstrap.min.css">
+      <!-- Typography CSS -->
+      <link rel="stylesheet" href="../css/typography.css">
+      <!-- Style CSS -->
+      <link rel="stylesheet" href="../css/style.css">
+      <!-- Responsive CSS -->
+      <link rel="stylesheet" href="../css/responsive.css">
+
+
+
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                  <h3 class="text-danger">Cloud<span class="text-primary ml-1">labs</span></h3> 
+                </a>
+              </div><!-- End Logo -->
+
+              <div class="card mb-3 ">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2 mb-2">
+                    <h4 class="card-title text-center pb-0 fs-4">Signup to continue</h4>
+                    <p class="text-center">Enter your details to signup</p>
                   </div>
-                  <div class="form-group">
-                     <label>Password</label>
-                     <input type="password" class="form-control" placeholder="Password">
-                  </div>
-                  <button type="submit" class="btn btn-black">Login</button>
-                  <button type="submit" class="btn btn-secondary">Register</button>
-               </form>
+
+                  <form method="post" class="row g-3 needs-validation" novalidate>
+
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Your Name</label>
+                      <div class="input-group has-validation">
+                        <input type="text" name="name" class="form-control" placeholder="Enter your name" id="yourUsername" required>
+                        <div class="invalid-feedback">Please input your name.</div>
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Username</label>
+                      <div class="input-group has-validation">
+                        <input type="text" name="username" class="form-control" placeholder="Enter your username" id="yourUsername" required>
+                        <div class="invalid-feedback">Please input your username.</div>
+                      </div>
+                    </div>
+                    <br>
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Email</label>
+                      <div class="input-group has-validation">
+                        <input type="text" name="email" class="form-control" placeholder="Enter your email" id="yourUsername" required>
+                        <div class="invalid-feedback">Please input your email</div>
+                      </div>
+                    </div>
+                    
+                    <div class="col-12">
+                      <label for="yourUsername" class="form-label">Phone</label>
+                      <div class="input-group has-validation">
+                        <input type="text" name="phone" class="form-control" id="yourUsername" placeholder="Enter your phone" required>
+                        <div class="invalid-feedback">Please input your username.</div>
+                      </div>
+                    </div>
+                    <br>
+
+                    <div class="col-12">
+                      <label for="yourPassword" class="form-label">Password</label>
+                      <input type="password" name="password" class="form-control" id="yourPassword" placeholder="Enter your password" required>
+                      <div class="invalid-feedback">Please enter your password!</div>
+                      <br>
+                    </div>
+                    
+<!-- 
+                    <div class="col-12">
+                      <div class="form-check">
+                        <input class="form-check-input" name="terms" type="checkbox" value="" id="acceptTerms" required>
+                        <label class="form-check-label" for="acceptTerms">I agree and accept the <a href="#">terms and conditions</a></label>
+                        <div class="invalid-feedback">You must agree before submitting.</div>
+                      </div>
+                    </div> -->
+
+                    <div class="col-12">
+                      <button class="btn btn-primary w-100" type="submit">Signup</button>
+                    </div>
+                    <div class="col-12">
+                     <br>
+                      <p class="small mb-0 text-center">Forgot your password? contact <a href="/contact_admin/">admin</a>.</p>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+
+
+              <div class="credits text-center">
+               Site Crafted and maintained by <a href="https://umarfarooq.online/">Umar Farooq</a>
+              </div>
+
             </div>
-         </div>
-      </div>
+          </div>
+        </div>
+
+      </section>
+
+    </div>
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="/assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="/assets/vendor/echarts/echarts.min.js"></script>
+  <script src="/assets/vendor/quill/quill.min.js"></script>
+  <script src="/assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="/assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="/assets/js/main.js"></script>
+
+</body>
+
+</html>
