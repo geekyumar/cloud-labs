9bdacd1aa4c0faac96734e308ee987f1be0bddce
Umar Farooq <umar.great.01@gmail.com>
2024-03-14 18:27:24 +0530

    concat and minify for css, watch task

--- workspace/grunt/Gruntfile.js
+++ workspace/grunt/Gruntfile.js
@@ -1,5 +1,43 @@
 module.exports = (grunt)=>{
-    grunt.registerTask('concat:css', ()=>{
-        console.log("Hello task is running..")
+    grunt.initConfig({
+        concat: {
+            options: {
+              separator: '\n',
+                 banner: '/* Concatenated by Grunt Project Runner */\n',
+            },
+            css: {
+              src: 'css/*.css',
+              dest: '../../htdocs/concat-css/style.css',
+            }
+          },
+
+          cssmin: {
+            css: {
+              files: [{
+                mergeIntoShorthands: false,
+                roundingPrecision: -1,
+                src: 'css/*.css',
+                dest: '../../htdocs/minify-css/style.min.css'
+              }]
+            }
+          },
+
+          watch: {
+            css: {
+              files: ['css/**/*.css'],
+              tasks: ['concat:css', 'cssmin'],
+              options: {
+                spawn: false,
+              },
+            },
+        }
+  
+  
     })
+
+    grunt.loadNpmTasks('grunt-contrib-concat')
+    grunt.loadNpmTasks('grunt-contrib-cssmin')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.registerTask('default', ['concat', 'cssmin', 'watch'])
+
 }
