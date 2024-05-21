4645ce0b92b1e6278f795c02e623b9302b70ce54
Umar Farooq <umar.great.01@gmail.com>
2024-05-21 23:31:49 +0530

    stop and createInstance api 21/05

---
+++ htdocs/src/api/labs/createInstance.php
@@ -0,0 +1,24 @@
+<?php
+
+${basename(__FILE__, '.php')} = function(){
+    if(REST::request_method() == 'POST' and session::get('session_token')) {
+        try {
+            if(usersession::validateSessionOwner(session::get('session_token'))){
+                $wg_ip = device::generateWgIP();
+                $private_ip = device::generatePrivateIP();
+                $create_instance = labs::create(session::$usersession->data['uid'], session::$usersession->data['username'], $private_ip, $wg_ip);
+                if($create_instance == true){
+                    REST::sendResponseData(200, ["response" => "success"]);
+                } else {
+                    REST::sendResponseData(500, ["response" => "failed"]);
+                }
+        } else {
+            REST::sendResponseData(500, ["response" => "auth_error"]);
+        }
+     } catch(Exception $e) {
+            REST::sendResponseData(500, ["response" => "error"]);
+        }
+    } else {
+        REST::sendResponseData(500, ["response" => "invalid_api_parameters"]);
+    }
+};
